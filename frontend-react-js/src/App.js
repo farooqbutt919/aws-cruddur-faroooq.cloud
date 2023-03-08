c4810778a88d1a82b58fe36a95b1672338425b57
Farooq Butt <106134852+farooqbutt919@users.noreply.github.com>
2023-03-08 09:01:19 +0400

    Update App.js

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
-import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -10,6 +9,7 @@ import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
+import process from 'process';
 import {
   createBrowserRouter,
   RouterProvider
@@ -32,15 +32,10 @@ Amplify.configure({
   }
 });
 
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeFeedPage />
-  },
-  {
-    path: "/notifications",
-    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",
